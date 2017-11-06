b89a1c091698ed5c41b6f710466092d21a2ff42b
Rohaan kathir <rohaankathir@users.noreply.github.com>
2017-11-06 17:06:40 +1100

    FormGroup

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -7,11 +7,33 @@ import { Customer } from './customer';
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit  {
+    customerForm: FormGroup;
+    // This is a data model
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    constructor(private fb: FormBuilder){}
+    
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: ,
+            email:'',
+            sendCatalog: true
+        });
+    }
+
+    populateTestData(): void{
+        this.customerForm.patchValue({
+            firstName: 'Rohaan',
+            lastName: 'Kathirgamathamby',
+            //email: 'rkath@hays.com.au',
+            sendCatalog: false
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
  }
