1b256df33092d968e1676f2789415e6ec4c02d26
Rohaan kathir <rohaankathir@users.noreply.github.com>
2017-11-07 17:13:20 +1100

    Validations

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+/*
+// Custom validation which does not accept parameters
+// Calling => rating: ['', ratingRange],
+function ratingRange (c: AbstractControl): {[key: string]: boolean} | null {
+    if(c.value != undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)){
+        return {'range': true};
+    }
+    return null;
+}
+*/
+
+// Custom validation which accepts parameters
+// Calling => rating: ['', ratingRange(1,5)],
+function ratingRange (min: number, max: number): ValidatorFn {
+    return  (c: AbstractControl): {[key: string]: boolean} | null => {
+        if(c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)){
+            return {'range': true};
+        }
+        return null;
+    }
+}
 
 @Component({
     selector: 'my-signup',
@@ -16,9 +38,12 @@ export class CustomerComponent implements OnInit  {
     
     ngOnInit(): void {
         this.customerForm = this.fb.group({
-            firstName: '',
-            lastName: ,
-            email:'',
+            firstName:['', [Validators.required, Validators.minLength(3)]],
+            lastName:['', [Validators.required, Validators.maxLength(50)]],
+            email:['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1,5)],
             sendCatalog: true
         });
     }
@@ -35,5 +60,16 @@ export class CustomerComponent implements OnInit  {
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void{
+        const phoneControl = this.customerForm.get('phone');
+        if(notifyVia === 'text'){
+            phoneControl.setValidators(Validators.required);
+        }
+        else{
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
  }
