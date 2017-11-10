c5699a25a2844b0a19109a6204b33e99ff4f5d2f
Rohaan kathir <rohaankathir@users.noreply.github.com>
2017-11-10 16:16:42 +1100

    group control changes

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -25,6 +25,23 @@ function ratingRange (min: number, max: number): ValidatorFn {
     }
 }
 
+// Email match validation
+// Paramter : c: FormGroup
+// Return :
+// key and value pair. Key - name of the validation rule. In our case 'match', value : true 
+function emailMatcher(c: AbstractControl){
+    let emailControl = c.get('email');
+    let confirmEmailControl = c.get('confirmEmail');
+    // If the controls are not touched, we don't have to return error
+    if(emailControl.pristine || confirmEmailControl.pristine){
+        return null;
+    }
+    if(emailControl.value === confirmEmailControl.value){
+        return null;
+    }
+    return { match: true }
+}
+
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
@@ -33,6 +50,13 @@ export class CustomerComponent implements OnInit  {
     customerForm: FormGroup;
     // This is a data model
     customer: Customer= new Customer();
+    emailMessage: string;
+    confirmEmailMessage: string;
+
+    private validationMessages = {
+        required: 'Please enter your email address.',
+        pattern: 'Please enter a valid email address.'
+    };
 
     constructor(private fb: FormBuilder){}
     
@@ -40,12 +64,21 @@ export class CustomerComponent implements OnInit  {
         this.customerForm = this.fb.group({
             firstName:['', [Validators.required, Validators.minLength(3)]],
             lastName:['', [Validators.required, Validators.maxLength(50)]],
-            email:['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            emailGroup: this.fb.group({
+                email:['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+                confirmEmail:['', Validators.required],   
+            }, { validator : emailMatcher }),                     
             phone: '',
             notification: 'email',
             rating: ['', ratingRange(1,5)],
             sendCatalog: true
         });
+
+        this.customerForm.get('notification').valueChanges
+            .subscribe(value => this.setNotification(value));
+
+        const emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.subscribe(value => this.setMessage(emailControl));
     }
 
     populateTestData(): void{
@@ -60,6 +93,15 @@ export class CustomerComponent implements OnInit  {
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.emailMessage = '';
+        if((c.touched || c.dirty) && c.errors){
+            // key is the validation rule name
+            this.emailMessage = Object.keys(c.errors).map(key=> 
+                this.validationMessages[key]).join(' ');
+        }
     }
 
     setNotification(notifyVia: string): void{
